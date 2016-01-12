86339956ca71a3b1d853d2afa5e99a86fd405a57
padraig08 <padraig08@live.com>
2016-01-12 17:36:08 -0500

    mocking out simple connection script

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-const irc = require('irc');
+const irc = require('irc'),
+	bonkConfig = require('./bonkConfig.json');
+
+const client = new irc.Client(bonkConfig.server, bonkConfig.nick, bonkConfig.options);
+
+client.connect();
