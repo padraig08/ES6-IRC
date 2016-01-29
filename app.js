2621de6789a4811f20d8c38901d5dbe06a037b56
padraig08 <padraig08@live.com>
2016-01-29 01:39:41 -0500

    removed connect function from bot

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const irc = require('irc'),
 	bonkConfig = require('./bonkConfig.json');
 
-const client = new irc.Client(bonkConfig.server, bonkConfig.nick, bonkConfig.options);
+const bot = new irc.Client(bonkConfig.server, bonkConfig.nick, bonkConfig.options);
 
-client.connect();
+bot.addListener('error', function(message) {
+    console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+});
+
+bot.addListener('message', function(from, to, message) {
+    console.log('%s => %s: %s', from, to, message);
+});
+bot.addListener('pm', function(nick, message) {
+    console.log('Got private message from %s: %s', nick, message);
+});
+bot.addListener('join', function(channel, who) {
+    console.log('%s has joined %s', who, channel);
+});
+bot.addListener('part', function(channel, who, reason) {
+    console.log('%s has left %s: %s', who, channel, reason);
+});
+bot.addListener('kick', function(channel, who, by, reason) {
+    console.log('%s was kicked from %s by %s: %s', who, channel, by, reason);
+});
